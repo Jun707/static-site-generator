a26bc4e7cbe37e0dc7919581bb68f4cd194a6705
Jun707 <juntowork@gmail.com>
2024-07-02 20:46:34 -0600

    add paragraph heading code block type conversion

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,7 @@
+from htmlnode import ParentNode, LeafNode
+from textnode import text_node_to_html_node
+from extract_markdown import text_to_textnodes
+
 block_type_paragraph = "paragraph"
 block_type_heading = "heading"
 block_type_code = "code"
@@ -7,15 +11,16 @@ block_type_ordered_list = "ordered_list"
 
 def markdown_to_blocks(markdown: str):
     sub_string = markdown.split("\n\n")
-    res =[]
+    res = []
     for s in sub_string:
         temp = s.strip()
         if temp:
             res.append(temp)
+    print(res)
     return res
 
-def block_to_block_type(markdown: str):
-    lines = markdown.split("\n")
+def block_to_block_type(block: str):
+    lines = block.split("\n")
     if lines[0].startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
         return block_type_heading
     if lines[0].startswith("```") and lines[-1].endswith("```"):
@@ -28,3 +33,54 @@ def block_to_block_type(markdown: str):
         if not lines[i].startswith(f"{i+1}. "):
             return block_type_paragraph
     return block_type_ordered_list
+
+def markdown_to_html_node(markdown: str):
+    blocks = markdown_to_blocks(markdown)
+    children = []
+    for block in blocks:
+        html_node = block_to_html_node(block)
+        children.append(html_node)
+    return ParentNode("div", children, None)
+
+def block_to_html_node(block: str):
+    block_type = block_to_block_type(block)
+    if block_type == block_type_paragraph:
+        return paragraph_block_type(block)
+    if block_type == block_type_heading:
+        return heading_block_type(block)
+
+
+def text_to_children(text: str):
+    text_nodes = text_to_textnodes(text)
+    children = []
+    for node in text_nodes:
+        html_node = text_node_to_html_node(node)
+        children.append(html_node)
+    return children
+
+
+def paragraph_block_type(block: str):
+    lines = block.split("\n")
+    paragraph = (" ").join(lines)
+    children = text_to_children(paragraph)
+    return ParentNode("p", children)
+
+def heading_block_type(block: str):
+    level = 0
+    for char in block:
+        if char == "#":
+            level +=1
+        else:
+            break
+    text = block[level+1:]
+    children = text_to_children(text)
+    
+    return ParentNode(f"h{level}", children)
+
+def code_block_type(block: str):
+    text = block[4: -3]
+    children = text_to_children(text)
+    code_block = ParentNode("code", children)
+    return ParentNode("pre", code_block)
+
+
--- src/htmlnode.py
+++ src/htmlnode.py
@@ -36,8 +36,8 @@ class LeafNode(HTMLNode):
 
 class ParentNode(HTMLNode):
 
-    def __init__(self, tag, value, children, props=None):
-        super().__init__(tag, None, children, props)
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, children, props)
 
     def to_html(self):
         if not self.tag:
